323f729d92c3f8ed35ab1dde351f0e3d5c7bdf28
CesaireD <cesairedenon08@gmail.com>
2023-03-24 11:13:09 +0100

    1

--- cesaire/app/Http/Controllers/HomeController.php
+++ cesaire/app/Http/Controllers/HomeController.php
@@ -62,7 +62,7 @@ class HomeController extends Controller
         return view('/ajouter');
     }
 
-    function ajoutPost(Request $request) {
+    public function ajoutPost(Request $request) {
         $request->validate(
             [
                 "nom"=>['required'],
@@ -82,5 +82,67 @@ class HomeController extends Controller
         ]);
         return redirect()->back()->with('statut', 'Voiture ajoutée!');
         // dd($request->all());
+    }
+
+    public function read() {
+        $lists = Car::all();
+        return view('admin.read', ['lists'=>$lists]);
+    }
+
+    public function update($id) {
+        $car = Car::findOrFail($id);
+        return view('admin.update', ['car'=>$car]);
+    }
+
+    public function updatePost($id, Request $request) {
+        $request->validate(
+            [
+                "nom"=>['required'],
+                "marque"=>['required'],
+                "description"=>['required'],
+                "prix"=>['required'],
+                "quantite"=>['required']
+            ]);
+        $path = $request->file('img')->store('images');
+
+        Car::where('id', $id)->update([
+            "nom"=>$request->nom,
+            "marque"=>$request->marque,
+            "photo"=>$path,
+            "description"=>$request->description,
+            "prixLocation"=>$request->prix,
+            "quantiteDispo"=>$request->quantite
+        ]);
+        return redirect()->back()->with('statut', 'Modification effectué!');
+    }
+
+    public function delete($id) {
+        Car::where('id', $id)->delete();
+        return redirect()->back()->with('statut', 'Suppression effectué!');
+    }
+
+    public function detail($id) {
+        $theCar = Car::findOrFail($id);
+        return view('frontend.detail', ['car'=> $theCar]);
+    }
+
+    public function detailPost($id, Request $request) {
+        $request->validate([
+            "debut"=>['required'],
+            "fin"=>['required'],
+        ]);
+        Location::create([
+            "dateLocation"=>$request->debut,
+            "dateFinLocation"=>$request->fin,
+            "restorer"=>0,
+            /*"user_id"=>$request,
+            "car_id"=>$request,*/
+        ]);
+        return redirect()->back()->with('statut', 'Votre demande a été approuvée');
+    }
+
+    public function offers() {
+        $allCars = Car::all();
+        return view('frontend.offers', ['allCars' => $allCars]);
     }
 }
